fa8c281a208b363c791302876ed520f4604614d0
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-01-25 09:17:31 +0000

    Creating new component

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Hello from './hello'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Test
+      <Hello/>
     </>
   )
 }
