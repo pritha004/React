b81975ea631a30f20925bcad6599ff8190bcd683
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-01-28 14:02:15 +0000

    Multiple setter method of useState (here setCounter)

--- 03counter/src/App.jsx
+++ 03counter/src/App.jsx
@@ -10,12 +10,21 @@ function App() {
 
   const increase=()=>{
     if(counter<20)
-    setCounter(counter+1)
+    {
+      //setCounter(counter+1)
+      // setCounter(counter+1) Wont update as useState sends updates in batches, and when it finds same operation done multiple times, it does it once; if ever needed this scenario use below syntax
+
+      setCounter((prevCounter)=>prevCounter+1) 
+      setCounter((prevCounter)=>prevCounter+1) //when used this method, callback waits for previous update and then updates the next setCounter statement
+      
+    }
   }
 
   const decrease=()=>{
     if(counter>0)
-    setCounter(counter-1)
+    {
+      setCounter(counter-1)
+    }
   }
 
   return (
