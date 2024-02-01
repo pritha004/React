f3f9b5eecd01063dc39bcc15674928bda3ce1632
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-02-01 07:12:50 +0000

    Created a custom hook to fetch currency data from api

---
+++ 08currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-orange-200'>Currency App</h1>
+    </>
+  )
+}
+
+export default App
