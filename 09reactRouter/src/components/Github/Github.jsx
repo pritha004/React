446dbfa818499db462f6106528e38038574c1877
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-02-06 11:41:49 +0000

    Loader in routes and useLoaderData hook

--- 09reactRouter/src/components/Github/Github.jsx
+++ 09reactRouter/src/components/Github/Github.jsx
@@ -1,24 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 
 const Github = () => {
+  const data=useLoaderData();
+  // const [data,setData]=useState([]);
 
-  const [data,setData]=useState([]);
-
-    useEffect(()=>{
-        fetch('https://api.github.com/users/pritha004')
-        .then(res=>res.json())
-        .then(data=>{
-          console.log(data);
-            setData(data)
-        })
-    },[])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/pritha004')
+    //     .then(res=>res.json())
+    //     .then(data=>{
+    //       console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
 
   return (
-    <div className='text-center m-4 bg-gray-600 text-white text-3xl'>Github Pulic Repos:{data.public_repos} 
+    <div className='text-center m-4 bg-gray-600 text-white text-3xl'>Github Public Repos:{data.public_repos} 
     <img src={data.avatar_url} width={200}/>
     </div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader=async()=>{
+        
+        const res= await fetch('https://api.github.com/users/pritha004');
+        return res.json();
+}
