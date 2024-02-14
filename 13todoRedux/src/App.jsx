5d28eae7675d895f4bd44c1b3734cc9d1c75e4ad
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-02-14 10:16:36 +0000

    Used useDispatch and useSelector to update and access store

--- 13todoRedux/src/App.jsx
+++ 13todoRedux/src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
 
   return (
     <>
-      <h1>Redux Toolkit</h1>
+      <h1 className='text-white text-xl'>Redux Toolkit TODO</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
