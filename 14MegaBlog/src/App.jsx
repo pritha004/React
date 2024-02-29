61e1ffa923a6150e716f246685303e5043d0b13d
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-02-29 10:00:56 +0000

    Added changes in store and wrapped app in Provider. Added tailwind css.

--- 14MegaBlog/src/App.jsx
+++ 14MegaBlog/src/App.jsx
@@ -1,9 +1,44 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import "./App.css";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <>
-      <h1>Blog app</h1>
+      {loading ? (
+        <>Loading...</>
+      ) : (
+        <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+          <div className="w-full block">
+            <Header/>
+            <main>
+              {/* <Outlet/> */}
+            </main>
+            <Footer/>
+          </div>
+        </div>
+      )}
     </>
   );
 }
