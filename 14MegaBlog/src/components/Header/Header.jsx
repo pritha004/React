33fe4cd341f5981b64e88f138d944f9be74578e1
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-02-29 15:17:19 +0000

    Added Header and Footer component

--- 14MegaBlog/src/components/Header/Header.jsx
+++ 14MegaBlog/src/components/Header/Header.jsx
@@ -1,6 +1,10 @@
-
+import {Container,Logo,LogoutBtn} from '../index';
+import {Link} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const authStatus=useSelector((state)=>state.auth.status)
   return (
     <div>Header</div>
   )
---
+++ 14MegaBlog/src/components/Logo.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+
+
+const Logo = (width='100px') => {
+  return (
+    <div>Logo</div>
+  )
+}
+
+export default Logo
