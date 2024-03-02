0cf99bfb292b0b8ee14d9bc93d3bbdc4d5577860
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-03-02 14:18:59 +0000

    Added components and Header Footer

--- 14MegaBlog/src/components/Logo.jsx
+++ 14MegaBlog/src/components/Logo.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
 
-const Logo = (width='100px') => {
+const Logo = ({width='100px'}) => {
   return (
     <div>Logo</div>
   )
