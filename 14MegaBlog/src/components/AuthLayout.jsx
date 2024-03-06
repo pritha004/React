4c866455f97c9891f7e1ba6f4be6bd04c76d4be2
pritha004 <115443887+pritha004@users.noreply.github.com>
2024-03-06 13:22:32 +0000

    Added routes and pages to the application

--- 14MegaBlog/src/components/AuthLayout.jsx
+++ 14MegaBlog/src/components/AuthLayout.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useEffect,useState } from "react";
+import { useEffect,useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
--- 14MegaBlog/src/components/index.js
+++ 14MegaBlog/src/components/index.js
@@ -8,6 +8,10 @@ import Signup from "./Signup";
 import Login from "./Login";
 import PostForm from "./PostForm/PostForm";
 import PostCard from './PostCard';
+import AuthLayout from './AuthLayout';
+import Button from './Button';
+import Input from './Input';
+import Select from './Select';
 
 export {
   Header,
@@ -19,5 +23,9 @@ export {
   Signup,
   Login,
   PostForm,
-  PostCard
+  PostCard,
+  AuthLayout,
+  Button,
+  Input,
+  Select
 };
